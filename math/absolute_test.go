4e95418478318b0759335a19fbd0c2005e69c195
Adhiana <adhianamastur48@gmail.com>
2022-12-13 21:14:34 +0700

    using subtest

--- math/absolute_test.go
+++ math/absolute_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 func TestAbsolute(t *testing.T) {
-	result := Absolute(-5)
+	t.Run("negative number", func(t *testing.T) {
+		result := Absolute(-5)
+		assert.Equal(t, 5, result, "expect 5, but got %d", result)
+	})
 
-	assert.Equal(t, 5, result, "expect 5, but got %d", result)
-}
-
-func TestAbsolute_WithPositiveNum(t *testing.T) {
-	result := Absolute(5)
-
-	assert.Equal(t, 5, result, "expect 5, but got %d", result)
+	t.Run("positive number", func(t *testing.T) {
+		result := Absolute(5)
+		assert.Equal(t, 5, result, "expect 5, but got %d", result)
+	})
 }
