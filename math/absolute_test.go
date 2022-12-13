898faa81d8c54d5e5fa3c9817cb3a1e183c91ad7
Adhiana <adhianamastur48@gmail.com>
2022-12-13 23:30:36 +0700

    short flag

--- math/absolute_test.go
+++ math/absolute_test.go
@@ -2,6 +2,7 @@ package math
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -21,4 +22,30 @@ func TestAbsolute(t *testing.T) {
 func TestAbsolute_FlakyTest(t *testing.T) {
 	// skip untuk testing yg kadang berhasil kadang gagal entah kenapa
 	t.Skip()
+}
+
+func TestAbsolute_Short(t *testing.T) {
+	/**
+	digunakan untuk skip testing:
+	- yang memakan waktu agak lama
+	- integration test
+
+	dijalankan dengan perintah
+		go test ./... -short
+	*/
+	if testing.Short() {
+		t.Skip()
+	}
+
+	<-time.After(5 * time.Second)
+
+	t.Run("negative number", func(t *testing.T) {
+		result := Absolute(-5)
+		assert.Equal(t, 5, result, "expect 5, but got %d", result)
+	})
+
+	t.Run("positive number", func(t *testing.T) {
+		result := Absolute(5)
+		assert.Equal(t, 5, result, "expect 5, but got %d", result)
+	})
 }
